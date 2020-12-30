4b2287de1cf6ecd70ead0fbe103968942e423ada
Jack Gibson <enozorez@gmail.com>
2020-12-30 16:47:11 +0000

    Add more detail to randomizer methods, comment code

---
+++ RandomItemGenerator/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace RandomItemGenerator
+{
+    class Weapon
+    {
+        public static string Randomize(String[] args)
+        {
+            Random rand = new Random();
+            int idx = rand.Next(args.Length);
+            string randomValue = args[idx];
+            return randomValue;
+        }
+        public static void Generator()
+        {
+            string[] prefixesList = { "Bronze", "Silver", "Golden" };
+            string[] weaponTypeList = { "Wand", "Sword", "Axe" };
+            string prefix = Randomize(prefixesList);
+            string weaponType = Randomize(weaponTypeList);
+
+            Console.WriteLine($"Selected {prefix + " " + weaponType}");
+            Console.ReadLine();
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Weapon.Generator();
+        }
+    }
+}
