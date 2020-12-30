d254d3d6e6d67ba15122f6039bfaed03c42c9e55
Jack Gibson <enozorez@gmail.com>
2020-12-30 16:49:31 +0000

    Add more detail to randomizer methods and add comments

--- RandomItemGenerator/Program.cs
+++ RandomItemGenerator/Program.cs
@@ -6,19 +6,47 @@ namespace RandomItemGenerator
     {
         public static string Randomize(String[] args)
         {
+            //A simple randomizer method, pass it an array of strings and it will return a random one.
             Random rand = new Random();
             int idx = rand.Next(args.Length);
             string randomValue = args[idx];
             return randomValue;
         }
+
+        public static string RandomizePrefix(String[] meleePrefixes, String[] rangedPrefixes, String WeaponType)
+        {
+            //A more complex randomizer method for rolling prefixes based on the weapon type, passed in the melee and ranged prefix arrays and the weapon type string
+            Random rand = new Random();
+            int idx;
+            string chosenPrefix = "";
+
+            switch (WeaponType)
+            {
+                case "Wand":
+                case "Bow":
+                    idx = rand.Next(rangedPrefixes.Length);
+                    chosenPrefix = rangedPrefixes[idx];
+                    break;
+                case "Sword":
+                case "Axe":
+                    idx = rand.Next(meleePrefixes.Length);
+                    chosenPrefix = meleePrefixes[idx];
+                    break;
+            }
+
+            return chosenPrefix;
+        }
+
         public static void Generator()
         {
-            string[] prefixesList = { "Bronze", "Silver", "Golden" };
-            string[] weaponTypeList = { "Wand", "Sword", "Axe" };
-            string prefix = Randomize(prefixesList);
+            //The generator method which holds the prefix arrays, weapon type array and calls the randomizer functions to build the name of the item.
+            string[] meleePrefixesList = { "Bronze", "Silver", "Golden" };
+            string[] rangedPrefixesList = { "Accurate", "Fast", "Homing" };
+            string[] weaponTypeList = { "Wand", "Sword", "Axe", "Bow" };
             string weaponType = Randomize(weaponTypeList);
+            string prefix = RandomizePrefix(meleePrefixesList, rangedPrefixesList, weaponType);
 
-            Console.WriteLine($"Selected {prefix + " " + weaponType}");
+            Console.WriteLine($"Generated: {prefix + " " + weaponType}");
             Console.ReadLine();
         }
     }
@@ -26,6 +54,7 @@ namespace RandomItemGenerator
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Random Item Generator v0.1 by Jack Gibson");
             Weapon.Generator();
         }
     }
